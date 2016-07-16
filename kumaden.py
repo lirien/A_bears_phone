aad5efcc68201aec019740bf0a95ed557e395c73
Erin Sullivan <kittylily@gmail.com>
2016-07-16 16:50:27 -0700

    Added kumaden

---
+++ kumaden.py
@@ -0,0 +1,20 @@
+#from matrix_keypad import RPi_GPIO
+from dialog_tree import DialogTree
+from pprint import pprint
+
+def main():
+  dialog_tree = DialogTree()
+  dialog_tree.load('dialog.json')
+
+  print dialog_tree.active_text
+
+  #kp = RPi_GPIO.keypad()
+
+def digit():
+  # Loop while waiting for a keypress
+  digitPressed = None
+  while digitPressed == None:
+    digitPressed = kp.getKey()
+  return digitPressed
+
+main()
